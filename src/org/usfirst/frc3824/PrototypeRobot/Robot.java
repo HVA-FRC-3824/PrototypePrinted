59bbdddf1a043f59115bb07240b420722d460247
Robert Palmer Jr <robert.palmer@ingenutec.com>
2016-01-16 12:04:59 -0500

    Working KOP robot code

--- src/org/usfirst/frc3824/PrototypeRobot/Robot.java
+++ src/org/usfirst/frc3824/PrototypeRobot/Robot.java
@@ -15,6 +15,8 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 import org.usfirst.frc3824.PrototypeRobot.commands.*;
 import org.usfirst.frc3824.PrototypeRobot.subsystems.*;
 
